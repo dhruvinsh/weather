5006a6a74b191f2cd1e2f38e6175dc44208b3261
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-12 18:12:55 -0400

    fix: tailwind css utility warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,tsx,jsx}"],
+  content: ["./src/**/*.{js,tsx,jsx,css}"],
   theme: {
     extend: {},
   },
